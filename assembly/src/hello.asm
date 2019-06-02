9b6cb6107af44a2ac3735749f11a5ee3a1ab4ac4
Saurabh Goyal <saurabh.goyal@healthify.co>
2019-06-02 12:10:10 +0530

    use macros

--- assembly/src/hello.asm
+++ assembly/src/hello.asm
@@ -1,17 +1,25 @@
 ; Coded for MacOS 64 bit, will not run in any other OS.
 
+%macro write_string 2
+    mov rax, 0x02000004     ; Sys call for write operation
+    mov rdi, 1              ; File handle for stdout is 1
+    mov rsi, %1             ; String to print
+    mov rdx, %2             ; Length of string to print
+    syscall                 ; Perform write
+%endmacro
+
+%macro exit 0
+    mov rax, 0x2000001 ; exit
+    mov rdi, 0
+    syscall
+%endmacro
+
 section .text
     global _main ; need to be declared for linker
 
 _main:
-  mov rax, 0x2000004 ; write
-  mov rdi, 1 ; stdout
-  mov rsi, msg
-  mov rdx, msg.len
-  syscall
-  mov rax, 0x2000001 ; exit
-  mov rdi, 0
-  syscall
+    write_string msg, msg.len
+    exit
 
 section .data
 msg:    db      "Hello, world!", 10
--- assembly/src/triangle.asm
+++ assembly/src/triangle.asm
@@ -1,6 +1,20 @@
 section .text
     global _main
 
+%macro write_string 2
+    mov rax, 0x02000004     ; Sys call for write operation
+    mov rdi, 1              ; File handle for stdout is 1
+    mov rsi, %1             ; String to print
+    mov rdx, %2             ; Length of string to print
+    syscall                 ; Perform write
+%endmacro
+
+%macro exit 0
+    mov rax, 0x2000001 ; exit
+    mov rdi, 0
+    syscall
+%endmacro
+
 _main:
     mov rdx, buffer    ; Buffer output
     mov r8, 1          ; Line number
@@ -19,17 +33,10 @@ lineDone:
     cmp r8, maxlines            ; compare current line number and expected line count
     jng line                   ; Print another line if line number <= expected line count
 done:
-    mov rax, 0x02000004     ; Sys call for write operation
-    mov rdi, 1              ; File handle for stdout is 1
-    mov rsi, buffer         ; Have output ready for print
-    mov rdx, buffersize     ; ???
-    syscall                 ; Perform write
-    mov rax, 0x02000001     ; Sys call for exit
-    xor rdi, rdi            ; Exit status code should be 0
-    syscall                 ; Perform exit
+    write_string buffer, buffersize      ; Print output
+    exit
 
 section .bss
 maxlines equ 8
-buffersize equ 44
+buffersize equ (maxlines * (maxlines + 3)) / 2        ; Number of stars for n lines + number of new line chars for each line.
 buffer: resb buffersize
-
